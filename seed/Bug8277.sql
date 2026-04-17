ca1acaa8be0620e554c9506f3c18a35138703cb2
thierry <--replace>
2026-04-17 12:46:55 +0300

    refactor: remove obsolete SQL seed files and migration scripts

--- seed/Bug8277.sql
+++
@@ -1,2 +0,0 @@
-CREATE INDEX idx_util_login ON lic_utilisateur(util_login);
-CREATE INDEX idx_auth_search ON lic_utilogauth(auth_ip, auth_datedemande, util_id);
--- seed/CDC7125.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE `lic_utilisateur` ADD `util_test_account` SMALLINT(1) NOT NULL DEFAULT '0' AFTER `util_idphotoged`;
-ALTER TABLE `auth_codes` ADD `util_id` INT NULL AFTER `manager_id`;
-
-
-ALTER TABLE `auth_codes` 
-ADD COLUMN `ac_failed_attempts` INT DEFAULT 0 COMMENT 'Nombre de tentatives échouées' 
-AFTER `ac_resend_count`;
